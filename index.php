e3095bbcd15a9a534e65ab43f2a5666fc4ee1881
RuslanPeka <35633105+RuslanPeka@users.noreply.github.com>
2021-03-22 20:33:37 +0200

    HW6.0

--- index.php
+++ index.php
@@ -13,108 +13,96 @@
 </div>
 <header>
     <div class="header_main_line">
-        <h1>Курс PHP - Занятие №5</h1>
+        <h1>Курс PHP - Занятие №6</h1>
     </div>
     <div class="header_second_line">
-        <h3>Классы</h3>
+        <h3>Классы и объекты. Часть 2</h3>
     </div>
 </header>
 <main>
     <section class="work">
         <?php
 
-        // 1. Создание абстрактного класса - Животные (Animals)
-        abstract class Animals
-        {
-            protected $animalsAll = [
-                'predator1' => 'Lion',
-                'predator2' => 'Tiger',
-                'predator3' => 'Wolf',
-                'herbivorous1' => 'Elephant',
-                'herbivorous2' => 'Cow',
-                'herbivorous3' => 'Hare',
-            ];
-
-            abstract public function listOfAnimals();
-        }
-
-        // 2. Создание абстрактного класса - Транспортные средства (Vehicles)
         abstract class Vehicles
         {
             protected $vehiclesAll = [
-                'boat1' => 'RedBoat',
-                'boat2' => 'BlueBoat',
-                'passengerCar1' => 'Mercedes',
-                'passengerCar2' => 'Audi',
-                'truck1' => 'MAN',
-                'truck2' => 'Scania',
+                'passengerCar_sedan' => 'Mercedes',
+                'passengerCar_hatchback' => 'Audi',
             ];
 
             abstract public function listOfVehicles();
         }
 
-        // 3. Создание наследников от животных - хищники (Predators), травоядные (Herbivores)
-        class Predators extends Animals
+        // 1) Создание интерфейсов: типа кузова (Bodywork); колёсная формула (Wheels); тип двигателя (Engine); тип коробки передач (Transmission)
+        interface Bodywork
         {
-            public function listOfAnimals()
-            {
-                $animals = $this->animalsAll;
-                foreach($animals as $type => $kind) {
-                    echo $type . ' => ' . $kind . '<br>';
-                }
-            }
+            public function kindOfBodywork();
+        }
 
-            public function listOfPredators()
+        interface Wheels
+        {
+            public function numOfWheels();
+        }
+
+        interface Engine
+        {
+            public function kindOfEngine();
+        }
+
+        interface Transmission
+        {
+            public function kindOfTransmission();
+        }
+
+        // 2) Создание трейта расчёта мощности двигателя, в зависимости от расхода воздуха (P = G / 3), где P - мощность, G - расход в-ха
+        trait EnginePower
+        {
+            public function powerOfEngine($consumption)
             {
-                $animals = $this->animalsAll;
-                foreach($animals as $type => $kind) {
-                    if(mb_substr_count($type, 'predator') > 0) echo $type . ' => ' . $kind . '<br>';
-                }
+                echo 'При расходе воздуха равном <b>' . $consumption . '</b> (кг) мощность двигателя составляет: <b>' . ($consumption / 3) . '</b> лошадиных сил';
             }
         }
 
-        class Herbivores extends Animals
+        class PassengerCars extends Vehicles implements Bodywork, Wheels, Engine, Transmission
         {
-            public function listOfAnimals()
-            {
-                $animals = $this->animalsAll;
-                foreach($animals as $type => $kind) {
-                    echo $type . ' => ' . $kind . '<br>';
-                }
-                        
-            }
-        
-            public function listOfHerbivores()
-            {
-                $animals = $this->animalsAll;
-                foreach($animals as $type => $kind) {
-                    if(mb_substr_count($type, 'herbivorous') > 0) echo $type . ' => ' . $kind . '<br>';
-                }
-            }
-        }
+            use EnginePower;
 
-        // 4. Создание наследников от транспортных средств - лодки (Boats), легковые авто (PassengerCars), грузовики (Trucks)
-        class Boats extends Vehicles
-        {
-            public function listOfVehicles()
+            public function kindOfBodywork()
             {
-                $vehicles = $this->vehiclesAll;
-                foreach($vehicles as $type => $kind) {
-                    echo $type . ' => ' . $kind . '<br>';
+                $cars = $this->vehiclesAll;
+                foreach($cars as $type => $kind) {
+                    if(mb_substr_count($type, 'sedan') > 0) echo 'У автомобиля <b>' . $kind . '</b> тип кузова - седан<br>';
+                    if(mb_substr_count($type, 'hatchback') > 0) echo 'У автомобиля <b>' . $kind . '</b> тип кузова - хэтчбек<br>';
                 }
             }
 
-            public function listOfBoats()
+            public function numOfWheels()
             {
-                $vehicles = $this->vehiclesAll;
-                foreach($vehicles as $type => $kind) {
-                    if(mb_substr_count($type, 'boat') > 0) echo $type . ' => ' . $kind . '<br>';
+                $cars = $this->vehiclesAll;
+                foreach($cars as $type => $kind) {
+                    if(mb_substr_count($type, 'sedan') > 0) echo 'У автомобиля <b>' . $kind . '</b> количество колёс - 4<br>';
+                    if(mb_substr_count($type, 'hatchback') > 0) echo 'У автомобиля <b>' . $kind . '</b> количество колёс - 4<br>';
                 }
             }
-        }
 
-        class PassengerCars extends Vehicles
-        {
+            public function kindOfEngine()
+            {
+                $cars = $this->vehiclesAll;
+                foreach($cars as $type => $kind) {
+                    if($kind == 'Mercedes') echo 'У автомобиля <b>' . $kind . '</b> двигатель - Mercedes-AMG M156<br>';
+                    if($kind == 'Audi') echo 'У автомобиля <b>' . $kind . '</b> двигатель - Audi 4.2 FSI<br>';
+                }
+            }
+
+            public function kindOfTransmission()
+            {
+                $cars = $this->vehiclesAll;
+                foreach($cars as $type => $kind) {
+                    if($kind == 'Mercedes') echo 'У автомобиля <b>' . $kind . '</b> коробка передач - Автоматическая<br>';
+                    if($kind == 'Audi') echo 'У автомобиля <b>' . $kind . '</b> коробка передач - Ручная<br>';
+                }
+            }
+
             public function listOfVehicles()
             {
                 $vehicles = $this->vehiclesAll;
@@ -132,108 +120,34 @@
             }
         }
 
-        class Trucks extends Vehicles
-        {
-            public function listOfVehicles()
-            {
-                $vehicles = $this->vehiclesAll;
-                foreach($vehicles as $type => $kind) {
-                    echo $type . ' => ' . $kind . '<br>';
-                }
-            }
-
-            public function listOfTrucks()
-            {
-                $vehicles = $this->vehiclesAll;
-                foreach($vehicles as $type => $kind) {
-                    if(mb_substr_count($type, 'truck') > 0) echo $type . ' => ' . $kind . '<br>';
-                }
-            }
-        }
-
-        // 5. Реализации всех наследников первого уровня созданы в соответствующих классха
-
-        // 6. Хелпер, работающий с массивами
-        class ArrayHelp
-        {
-            static function ArrayPrint(array $array)
-            {
-                echo '<pre>';
-                print_r($array);
-                echo '</pre>';
-            }
-        }
-
-        // 7. Хелпер, работающий со строками
-        class StringHelp
-        {
-            static function StringBold(string $str)
-            {
-                echo '<b>' . $str . '</b>';
-            }
-        }
-
         /*
             Вывод результатов
         */
 
-        // 1) Проверка работоспособности вывода списка всех животных
-        echo '<b>1. Список всех животных:</b><br>';
-        $predators = new Predators;
-        $predators->listOfAnimals();        // Метод отработал верно
+        // 1. Реализация интерфейса Bodywork
+        echo '<h3>1. Тип кузова:</h3>';
+        $car = new PassengerCars;
+        $car->kindOfBodywork();
         echo '<br><hr><br>';
 
-        // 2) Проверка работоспособности вывода списка всех хищников
-        echo '<b>2. Список всех хищников:</b><br>';
-        $predators->listOfPredators();      // Метод отработал верно
+        // 2. Реализация интерфейса Wheels
+        echo '<h3>2. Количество колёс:</h3>';
+        $car->numOfWheels();
         echo '<br><hr><br>';
 
-        // 3) Проверка работоспособности вывода списка всех животных
-        echo '<b>3. Список всех травоядных:</b><br>';
-        $herbivores = new Herbivores;
-        $herbivores->listOfHerbivores();    // Метод отработал верно
+        // 3. Реализация интерфейса Engine
+        echo '<h3>3. Тип двигателя:</h3>';
+        $car->kindOfEngine();
         echo '<br><hr><br>';
 
-        // 4) Проверка работоспособности вывода списка всех транспортных средств
-        echo '<b>4. Список всех транспортных средств:</b><br>';
-        $boats = new Boats;
-        $boats->listOfVehicles();           // Метод отработал верно
+        // 4. Реализация интерфейса Engine
+        echo '<h3>4. Тип двигателя:</h3>';
+        $car->kindOfTransmission();
         echo '<br><hr><br>';
 
-        // 5) Проверка работоспособности вывода списка всех лодок
-        echo '<b>5. Список всех лодок:</b><br>';
-        $boats->listOfBoats();              // Метод отработал верно
-        echo '<br><hr><br>';
-
-        // 6) Проверка работоспособности вывода списка всех легковых автомобилей
-        echo '<b>6. Список всех легковых автомобилей:</b><br>';
-        $passengerCar = new PassengerCars;
-        $passengerCar->listOfPassengerCars();       // Метод отработал верно
-        echo '<br><hr><br>';
-
-        // 7) Проверка работоспособности вывода списка всех грузовиков
-        echo '<b>7. Список всех грузовиков:</b><br>';
-        $trucks = new Trucks;
-        $trucks->listOfTrucks();                    // Метод отработал верно
-        echo '<br><hr><br>';
-
-        // 8) Проверка работоспособности хелпера для работы с массивами
-        $countries = [
-            'NorthAmerica1' => 'USA',
-            'NorthAmerica2' => 'Сanada',
-            'Europe1' => 'Germany',
-            'Europe2' => 'France',
-            'Asia1' => 'China',
-            'Asia2' => 'South Korea',
-        ];
-        echo '<b>8. Пример работы хелпера для работы с массивами:</b><br>';
-        ArrayHelp::ArrayPrint($countries);          // Отработал верно
-        echo '<br><hr><br>';
-
-        // 9) Проверка работоспособности хелпера для работы со строками
-        $someString = 'Я учусь создавать web-сайты.';
-        echo '<b>9. Пример работы хелпера для работы со строками:</b><br>';
-        StringHelp::StringBold($someString);        // Отработал верно
+        // 5. Реализация трейта
+        echo '<h3>5. Расчёт мощности двигателя:</h3>';
+        $car->powerOfEngine(1950);
         echo '<br><br><hr><br>';
 
         ?>
