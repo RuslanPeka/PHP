f7df875c3f493c459a4758ccb9c8db720ebabd8e
RuslanPeka <35633105+RuslanPeka@users.noreply.github.com>
2021-04-26 21:27:49 +0300

    commitFacade

--- index.php
+++ index.php
@@ -1,30 +1,20 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-<div class="line">
-    <p>ЛУЧШИЙ В МИРЕ САЙТ</p>
-</div>
-<header>
-    <div class="header_main_line">
-        <h1>Курс PHP - Занятие №3</h1>
-    </div>
-    <div class="header_second_line">
-        <h3>Функции</h3>
-    </div>
-</header>
-<main>
-    <section class="work">
-        <?php
+<?php
+    class Facade
+    {
+        private $requestPath;
 
-        ?>
-    </section>
-</main>
-</body>
-</html>
+        public function __construct()
+        {
+            $this->requestPath = $_SERVER['PATH_INFO'] ?? '/';
+        }
+
+        public function connectApi(string $actionName)
+        {
+            if(in_array($this->requestPath, 'api')) {
+                $classNamespace = 'Routes\\Api';
+                $this->classObj = new $classNamespace;
+                call_user_func(array($this->classObj, $actionName));
+            }
+        }
+    }
+?>
